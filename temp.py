8000ea4f00cc47d6e29f203fd1d23ece7a5b3548
Thussain0818 <67605807+Thussain0818@users.noreply.github.com>
2020-07-08 15:07:32 -0400

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,29 @@
+ # -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+print ("Hello World")
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+character_name = "Tom"
+character_age = "50"
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+print("He really like the name " + character_name + ",")
+print("but didn't like being " + character_age + ".")
+
+phrase = "Giraffe Academy"
+print("Giraffe\"Academy")
+print(phrase = "is cool")
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
